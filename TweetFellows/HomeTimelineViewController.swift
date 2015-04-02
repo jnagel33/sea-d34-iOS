b927d491d517c6e833586579cdaa3dc72513c235
Josh Nagel <jnagel33@gmail.com>
2015-04-01 23:12:59 -0700

    Fixed scrolling issue

--- TweetFellows/HomeTimelineViewController.swift
+++ TweetFellows/HomeTimelineViewController.swift
@@ -24,7 +24,6 @@ class HomeTimelineViewController: UIViewController, UITableViewDataSource, UITab
       self.tableView.delegate = self
       self.activityIndicator.startAnimating()
       self.tableView.userInteractionEnabled = false
-//      self.edgesForExtendedLayout = UIRectEdge.None
       
       self.tableView.alpha = 0
       UIView.animateWithDuration(2.0, animations: { () -> Void in
