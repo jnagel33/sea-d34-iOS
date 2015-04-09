fbf167dad2308d8fc10a111acca9df8690adc896
Josh Nagel <jnagel33@gmail.com>
2015-04-08 20:11:06 -0700

    Added

---
+++ ImageFilters/ImageFilters/ParseService.swift
@@ -0,0 +1,33 @@
+//
+//  ParseService.swift
+//  ImageFilters
+//
+//  Created by Josh Nagel on 4/7/15.
+//  Copyright (c) 2015 jnagel. All rights reserved.
+//
+
+import Foundation
+
+class ParseService {
+  
+  class func uploadImage(image: UIImage, size: CGSize, completionHandler: (String?) -> Void) {
+    let resizedImage = ImageResizer.resizeImage(image, size: size)
+    let imageData = UIImageJPEGRepresentation(image, 1.0)
+    let imageFile = PFFile(name: "post.jpg", data: imageData)
+    let post = PFObject(className: "Post")
+    post["imageFile"] = imageFile
+    
+    post.saveInBackgroundWithBlock { (finished, error) -> Void in
+      if error != nil {
+        //handle error
+      } else {
+        println("Successful upload")
+        NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+          completionHandler(nil)
+        })
+
+      }
+    }
+  }
+  
+}
